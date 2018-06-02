a41a0d456ce4d8e62451f9aeb1ea6e90f40b1a44
Maor Segev <37215368+maorseg@users.noreply.github.com>
2018-06-02 22:59:54 +0300

    Update PublicClassSeven.java

--- javacore/accessmodifiers/PublicClassSeven.java
+++ javacore/accessmodifiers/PublicClassSeven.java
@@ -11,7 +11,5 @@ public class PublicClassSeven {
         // Trying to access the public method of PublicClassSix
 
         obj.myMethod(10);
-
-        // We tried the same example with Default and we faced an error. Hope you remember
     }
 }
