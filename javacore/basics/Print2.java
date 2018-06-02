4e96ff23d77dcbec344277a3fa8029961b187723
Maor Segev <37215368+maorseg@users.noreply.github.com>
2018-06-02 03:31:57 +0300

    Update Print2.java

--- javacore/basics/Print2.java
+++ javacore/basics/Print2.java
@@ -7,12 +7,14 @@ package corejava.basics;
 public class Print2 {
 
     // Declaring a variable named website and print some text and go to new line
-    static String website = "www.google.com";
+    static String javaWeb = "https://www.java.com";
+    static String pythonWeb = "https://www.python.org";
 
     public static void main(String [] args){
+
         /* Print text and go to new line
          we use "+" to concatenate website with the given text*/
-
-        System.out.println("Learning Java from " + website);
+        System.out.println("Start Java from " + javaWeb);
+        System.out.println("Start python from " + pythonWeb);
     }
 }
