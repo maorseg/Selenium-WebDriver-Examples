40cdcf640c5463c299b4f9c92eeff000e70b4152
Maor Segev <37215368+maorseg@users.noreply.github.com>
2018-06-02 22:52:12 +0300

    Update ProtectedClassFive.java

--- javacore/accessmodifiers/ProtectedClassFive.java
+++ javacore/accessmodifiers/ProtectedClassFive.java
@@ -3,11 +3,11 @@ package corejava.accessmodifiers;
 /**
  * Created by Maor on 5/25/2018.
  */
-public class ProtectedClassFive {
+public class ProtectedClassFive extends ProtectedClassFour {
 
     public static void main(String args[]){
         ProtectedClassFive obj = new ProtectedClassFive();
-   //     System.out.println(obj.myMethod(100));
+        System.out.println(obj.myMethod(100));
     }
 
 }
