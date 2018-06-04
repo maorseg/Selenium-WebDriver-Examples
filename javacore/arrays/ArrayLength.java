1bdaafc65d04e282366ced6571d25615e9055ac2
Maor Segev <37215368+maorseg@users.noreply.github.com>
2018-06-04 22:41:12 +0300

    Add files via upload

---
+++ javacore/arrays/ArrayLength.java
@@ -0,0 +1,19 @@
+package corejava.arrays;
+
+/**
+ * Created by Maor on 6/4/2018.
+ */
+
+// Finding Length of Array:
+public class ArrayLength {
+
+    public static void main(String[] args){
+
+        // Create an array of size 5 integers
+        int[] arr = new int[50];
+
+        // 'length' is a function in Java which gives you the size of an array
+        System.out.println(arr.length);
+
+    }
+}
