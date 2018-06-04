def7b2f91bd27336d57ca16b3aaad6c9d5fa14f3
Maor Segev <37215368+maorseg@users.noreply.github.com>
2018-06-04 22:50:18 +0300

    Add files via upload

---
+++ javacore/arrays/ArrayEnhancedForLoop.java
@@ -0,0 +1,17 @@
+package corejava.arrays;
+
+/**
+ * Created by Maor on 5/26/2018.
+ */
+
+public class ArrayEnhancedForLoop {
+
+    public static void main(String[] args) {
+
+        int[] arr = {11,22,33,44,55};
+
+        for(int a:arr){
+            System.out.println(a);
+        }
+    }
+}
